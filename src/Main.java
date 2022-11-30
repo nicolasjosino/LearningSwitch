dc1a5769bb566496d757665a256888ddd9b31bfb
Nicolas Josino <nicolasjosino@gmail.com>
2022-11-29 23:48:22 -0300

    Ajuste main

--- src/Main.java
+++ src/Main.java
@@ -20,26 +20,26 @@ public class Main {
     public static void main(String[] args) {
         var switch1 = new Switch(2);
         var switch2 = new Switch(3);
-        Host h1 = new Host("h1", "10.15.20.1");
-        Host h2 = new Host("h2", "10.15.21.2");
-        Host h3 = new Host("h3", "10.15.21.3");
+        Host h1 = new Host("h1", "10.10.20.1");
+        Host h2 = new Host("h2", "10.10.21.2");
+        Host h3 = new Host("h3", "10.10.21.3");
         Router r1 = new Router(2, "r1");
         Router r2 = new Router(2, "r2");
 
         connect(h1, switch1);
         connect(switch1, r1);
 
-        r1.addRoute("10.15.20.0/24", "10.20.20.2", r1.getPorts().get(0));
-        r1.addRoute("10.15.21.0/24", "10.20.21.4", r1.getPorts().get(1));
+        r1.addRoute("10.10.20.0/24", "10.10.20.2", r1.getPorts().get(0));
+        r1.addRoute("10.10.21.0/24", "10.10.21.4", r1.getPorts().get(1));
 
         connect(r1, r2);
-
-        r2.addRoute("10.20.21.0/24", "10.20.21.5", r2.getPorts().get(0));
-        r2.addRoute("10.20.20.0/24", "10.20.20.3", r2.getPorts().get(1));
 
         connect(r2, switch2);
         connect(h2, switch2);
         connect(h3, switch2);
+
+        r2.addRoute("10.10.20.0/24", "10.10.20.5", r2.getPorts().get(0));
+        r2.addRoute("10.10.21.0/24", "10.10.21.3", r2.getPorts().get(1));
 
         h1.sendPacket(h2.getIpAddress(), "hello");
     }
